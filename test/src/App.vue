6bcbc2038db88df4b1823b5e3aeb0e3bcd179e81
Laury <laury.balard@gmail.com>
2019-08-21 09:48:18 +0200

    Ajout des fichiers carousels

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
     <NavBarre class="nav" />
-      <div class="div" key="div">
-        <PartOne />
-      </div>
-      <div id="div1" key="div1"></div>
-      <div id="div2" key="div2">
-        <Cards />
-      </div>
-      <div id="div3" key="div3"></div>
+    <div class="div" key="div">
+      <PartOne />
+      <!-- <carousel>
+        <carousel-slide v-for="n in slides" :key="n" :index="n-1"> -->
+          <!-- <div style="position:absolute; left:0; right:0; text-align:center; top:80%; ">
+          <p>Bonjour</p>
+          </div>-->
+          <!-- <img src="./assets/logo2.png" alt />
+        </carousel-slide>
+      </carousel> -->
+    </div>
+    <div id="div1" key="div1"></div>
+    <div id="div2" key="div2">
+      <Cards />
+    </div>
+    <div id="div3" key="div3"></div>
   </div>
 </template>
 
@@ -16,13 +24,22 @@
 import PartOne from "./components/PartOne";
 import NavBarre from "./components/NavBarre";
 import Cards from "./components/Cards";
+import Carousel from "./components/Carousel";
+import CarouselSide from "./components/CarouselSlide";
 
 export default {
   name: "App",
   components: {
     PartOne,
     NavBarre,
-    Cards
+    Cards,
+    Carousel,
+    CarouselSide
+  },
+  data() {
+    return {
+      slides: 3,  
+    }
   }
 };
 </script>
--- test/src/components/Carousel.vue
+++ test/src/components/Carousel.vue
@@ -1,13 +1,103 @@
 <template>
   <div>
     <h2>Carousel</h2>
+    <div class="carousel">
+    <slot></slot>
+    <button class="carousel__nav carousel__prev" @click.prevent="prev"></button>
+    <button class="carousel__nav carousel__next" @click.prevent="next"></button>
+    <div class="pagination">
+    <button v-for="n in slidesCount" @click="goto(n-1)" :key="n" :class="{active: n-1 === index}"></button>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {};
+  data () {
+    return {
+      index: 0,
+      slides: [],
+      direction: null
+    }
+  },
+  mounted () {
+    this.slides = this.$children
+    this.slides.forEach((slide, i) => {
+      slide.index = i
+    })
+  },
+  watch: {
+    slides (slides) {
+      if (this.index >= this.slidesCount) {
+        this.index = this.slidesCount - 1 
+      }
+    }
+  },
+  computed: {
+    slidesCount () {
+      return this.slides.length
+    }
+  },
+  methods: {
+    next () {
+      this.index++
+      this.direction = 'right'
+      if (this.index >= this.slidesCount) {
+        this.index = 0
+      }
+    },
+    prev () {
+      this.index--
+      this.direction = 'left'
+      if (this.index < 0) {
+        this.index = this.slidesCount - 1
+      }
+    },
+    goto (index) {
+      this.direction = index > this.index ? 'right' : 'left'
+      this.index = index
+    }
   }
-};
+}
 </script>
+<style>
+.carousel{
+  position: relative;
+}
+.carousel__nav{
+  position: absolute;
+  top: 50%;
+  left: 1em;
+  background: #000;
+  width: 60px;
+  height: 60px;
+  border: none;
+}
+.carousel__nav.carousel__next{
+  right: 1em;
+  left: auto;
+  background:  #000;
+}
+.pagination{
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+.pagination button{
+  display: inline-block;
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  border-style: none;
+  background-color: #000;
+  border-color: #000;
+  opacity: 0.8;
+  margin: 0 1.5px;
+}
+.pagination button.active{
+  background: rgb(128, 128, 128);
+}
+</style>
